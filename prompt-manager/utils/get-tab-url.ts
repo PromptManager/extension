38a2e64733fcf339ef50da08cd8dcb23ca2737ae
barnesrj <116855852+barnesrj@users.noreply.github.com>
2025-04-12 18:23:54 +0300

    Create get-tab-url.ts
    created file to get website URL

---
+++ prompt-manager/utils/get-tab-url.ts
@@ -0,0 +1,12 @@
+export const getCurrentTabUrl = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      if (tab?.url) {
+        resolve(tab.url);
+      } else {
+        reject("No active tab URL found.");
+      }
+    });
+  });
+};
